10fe8386d18184755f6d050ab20574c5efb59a25
nswbmw1992 <nswbmw1992@gmail.com>
2015-04-04 22:29:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## co-cache
+
+Cache GeneratorFunction result, only support `function`, `string`, `number` and `boolean` as parameters.
+
+### Install
+
+```
+npm i co-cache --save
+```
+
+### Usage
+
+```
+cache(GeneratorFunction, ms)
+```
+
+### Example
+
+```
+var cache = require('co-cache');
+
+var getTopicsByTab = cache(function* (tab, p) {
+  var query = {};
+  if (tab) { query.tab = tab; }
+  p = p || 1;
+  return yield Topic.find(query).skip((p - 1) * 10).limit(10).exec();
+}, 10000);
+
+co(function* () {
+  var topics = yield getTopicsByTab('question', 2);
+  ...
+}).catch(onerror);
+```
+
+### License
+
+MIT
---
+++ index.js
@@ -0,0 +1,39 @@
+var memoryCache = require('memory-cache');
+
+/**
+ * Cache GeneratorFunction result
+ * 
+ * @param {GeneratorFunction} genFunc
+ * @param {Number} ms
+ * @return {Any} result
+ * @api public
+ */
+
+module.exports = function (genFunc, time) {
+  if (!time || ('number' !== typeof time)) {
+    return genFunc;
+  }
+  return function* cache() {
+    var args =  Array.prototype.slice.call(arguments);
+    var cacheKey = args.map(function (arg) {
+      switch (typeof arg) {
+      case 'function':
+        return arg.name;
+      case 'string':
+      case 'number':
+      case 'boolean':
+        return arg.toString();
+      default:
+        throw new Error('co-cache only support `function`, `string`, `number` and `boolean` as parameters');
+      }
+    }).join('');
+
+    var result = memoryCache.get(cacheKey);
+    if (result) {
+      return result;
+    }
+    result = yield genFunc.apply(this, args);
+    memoryCache.put(cacheKey, result, time);
+    return result;
+  };
+};
